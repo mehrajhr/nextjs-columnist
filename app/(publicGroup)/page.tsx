60829a7ff3806b0f3d473863d8b03ca225382a23
Mehraj Hasan Riaj <mehrajhasanriajcse@gmail.com>
2026-09-24 23:24:26 +0600

    integreting shadcn ui

--- app/(publicGroup)/page.tsx
+++ app/(publicGroup)/page.tsx
@@ -1,7 +1,11 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 const HomePage = () => {
-  return <div></div>;
+  return <div>
+    <h1>Hello world</h1>
+    <Button size={"xs"}>Click me</Button>
+  </div>;
 };
 
 export default HomePage;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,15 @@
 import "./globals.css";
+import { Outfit, Oxanium } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const oxaniumHeading = Oxanium({subsets:['latin'],variable:'--font-heading'});
+
+const outfit = Outfit({subsets:['latin'],variable:'--font-sans'});
+
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html lang="en" className={`h-full antialiased`}>
+    <html lang="en" className={cn("h-full", "antialiased", "font-sans", outfit.variable, oxaniumHeading.variable)}>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
