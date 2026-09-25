c2b219104fbe3ae43f7857a66cbaeaa41698629b
Mehraj Hasan Riaj <mehrajhasanriajcse@gmail.com>
2026-09-26 00:20:55 +0600

    setting cookie in nextjs cookie

--- app/(authGroup)/_actions/authAction.ts
+++ app/(authGroup)/_actions/authAction.ts
@@ -1,5 +1,7 @@
 "use server";
 
+import { cookies } from "next/headers";
+
 type LoginState = {
   success: boolean;
   statusCode: number;
@@ -15,7 +17,7 @@ export const loginAction = async (
   formData: FormData,
 ) => {
   //   console.log(fromData);
-  console.log(prevState , "Prevstate");
+  console.log(prevState, "Prevstate");
 
   const email = formData.get("email");
   const password = formData.get("password");
@@ -35,9 +37,26 @@ export const loginAction = async (
     body: JSON.stringify(payload),
   });
 
-  const result = await res.json();
+  const result: LoginState = await res.json();
 
   console.log(result);
+
+  if (result.success) {
+    const cookieStore = await cookies();
+
+    cookieStore.set("accessToken", result.data.accessToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "none",
+      maxAge: 1000 * 60 * 60 * 24,
+    });
+    cookieStore.set("refreshToken", result.data.refreshToken, {
+      httpOnly: true,
+      secure: false,
+      sameSite: "none",
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    });
+  }
 
   return result;
 };
